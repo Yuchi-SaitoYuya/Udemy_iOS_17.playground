5276dc69a8f28df99eef397dd4f6020f132f594e
Yuchinante <ysuu45143745@gmail.com>
2025-04-25 17:27:23 +0900

    練習問題：5の倍数を出力するプログラム

--- Contents.swift
+++ Contents.swift
@@ -1,21 +1,21 @@
-//★練習問題：乗車判定プログラム
+//★練習問題：5の倍数を出力するプログラム
 //次のプログラムを if文 を使って作成してください。
-//【乗車判定プログラム】
-//年齢が10歳以上かつ身長が130cm以上の場合「乗車できます」と出力する
-//上記以外の場合は「乗車できません」と出力する
+//【5の倍数を出力するプログラム】
+//1から100までの数字の中で、5の倍数の数字のみ出力する
 
 import Foundation
 
-class Exercise {
 
-    func judgeBoarding(age: Int, height: Int) {
-        if age >= 10 && height >= 130 {
-            print("乗車できます")
-        } else {
-            print("乗車できません")
+class Exercise2 {
+    func printFiveMultiple() {
+
+        for i in 1...100 {
+            if i % 5 == 0 {
+                print(i)
+            }
         }
     }
 }
 
-let ex = Exercise()
-ex.judgeBoarding(age: 10, height: 130)
+let ex2 = Exercise2()
+ex2.printFiveMultiple()
