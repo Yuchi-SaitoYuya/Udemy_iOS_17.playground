dc7f9532074e72f2403e102b10a8c0b02f69c2fd
Yuchinante <ysuu45143745@gmail.com>
2025-04-28 11:53:30 +0900

    桁数判定プログラム

--- Contents.swift
+++ Contents.swift
@@ -1,21 +1,31 @@
-//★練習問題：5の倍数を出力するプログラム
-//次のプログラムを if文 を使って作成してください。
-//【5の倍数を出力するプログラム】
-//1から100までの数字の中で、5の倍数の数字のみ出力する
+//★練習問題：桁数判定プログラム
+//次のプログラムを switch文 を使って作成してください。
+//【桁数判定プログラム】
+//引数で受け取った数値の桁数を判定し「○桁の数値」と出力する
+//例：12 → 2桁の整数、256 → 3桁の数値
+//判定する桁数は、1桁から3桁まで
+//それ以外の場合は「範囲外の数値」と出力する
 
 import Foundation
 
-
-class Exercise2 {
-    func printFiveMultiple() {
-
-        for i in 1...100 {
-            if i % 5 == 0 {
-                print(i)
-            }
+class Exercise {
+    func printNumOfDigits(number: Int) {
+        let digitCount = String(number).count
+        switch digitCount {
+        case 1:
+            print("1桁の数値")
+        case 2:
+            print("2桁の数値")
+        case 3:
+            print("3桁の数値")
+        default:
+            print("範囲外の数値")
         }
     }
 }
 
-let ex2 = Exercise2()
-ex2.printFiveMultiple()
+let ex = Exercise()
+ex.printNumOfDigits(number: 5)   // 1桁の数値
+ex.printNumOfDigits(number: 23)  // 2桁の数値
+ex.printNumOfDigits(number: 999) // 3桁の数値
+ex.printNumOfDigits(number: 1234) // 範囲外の数値
